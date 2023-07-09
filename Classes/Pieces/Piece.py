3fda7219e58c93ffd78f7a5aba088c72e04f506d
MMD_PROJECT <mmdvioleti@gmail.com>
2023-07-09 23:12:07 +0430

    added comments to methods

--- Classes/Pieces/Piece.py
+++ Classes/Pieces/Piece.py
@@ -13,36 +13,38 @@ class Piece:
         self.j = j
         self.is_white = is_white
 
+    # This method places a piece on the board
     def place_on_board(self, board: Board):
+        # Calling board method to place the piece on itself
         board.place_on_board(self.i, self.j, self)
 
+    # This method moves a piece on the chess-board
     def move_to_position(self, board: Board, new_i: int, new_j: int):
+        # Calling board method to empty the square in itself
         board.empty_square(self.i, self.j)
+        # Renewing the ally position
         self.i = new_i
         self.j = new_j
+        # Placing the piece on the new position
         board.place_on_board(self.i, self.j, self)
 
+    # This method capures an enemy piece
     def capture(self, board : Board, enemy_i : int, enemy_j : int, enemy_piece : 'Piece'):
+        # Empty both ally square and enemy square first
         board.empty_square(self.i, self.j)
         board.empty_square(enemy_i, enemy_j)
-
+        # Renewing the ally position on the board
         self.i = enemy_i
         self.j = enemy_j
-
+        # Calling board method to place the ally piece on the new position
         board.place_on_board(self.i, self.j, self)
-
+        # Releasing the memory that has been taken by enemy piece 
         del enemy_piece 
         
 
     # This should be overrided in child classes
     def get_moving_poses(self):
         pass
-
-    def __str__(self):
-        return f"peice with i:{self.i} j:{self.j} and is_white{self.is_white}"
     
     def __call__(self):
         return f"peice with i:{self.i} j:{self.j} and is_white{self.is_white}"
-
-
-        
