a3fc383314370c7198b6044456401cfaae5c16a6
MMD_PROJECT <mmdvioleti@gmail.com>
2023-07-10 05:15:13 +0430

    fixed rook not checking if there is a blocking piece on the square or not to move

--- Classes/Pieces/rook.py
+++ Classes/Pieces/rook.py
@@ -1,39 +1,46 @@
-from typing import List
+import sys
+import os
+# Adding the path of parent directory (Classes) to the paths
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir)))
+
 import piece
+import board
+
+from typing import List
 
 class Rook(piece.Piece):
     def __init__(self, i: int, j: int, is_white: bool):
         super().__init__(i, j, is_white)
     
     # Override
-    def get_allowed_poses(self) -> List[List]:
+    def get_allowed_poses(self, board: board.Board) -> List[List]:
         allowed_moves = []
         
         # First options are to move up
         i = self.i - 1
         j = self.j
-        while 0 <= i <= 8:
+        while 0 <= i <= 8 and not board.is_piece_at_pos(i, j):
             allowed_moves.append([i, j])
             i -= 1
         
         # Second options are to move down
         i = self.i + 1
         j = self.j
-        while 0 <= i <= 8:
+        while 0 <= i <= 8 and not board.is_piece_at_pos(i, j):
             allowed_moves.append([i, j])
             i += 1
 
         # First options are to move right
         i = self.i
         j = self.j + 1
-        while 0 <= j <= 8:
+        while 0 <= j <= 8 and not board.is_piece_at_pos(i, j):
             allowed_moves.append([i, j])
             j += 1
         
         # First options are to move up
         i = self.i
         j = self.j - 1
-        while 0 <= j <= 8:
+        while 0 <= j <= 8 and not board.is_piece_at_pos(i, j):
             allowed_moves.append([i, j])
             j -= 1
 
